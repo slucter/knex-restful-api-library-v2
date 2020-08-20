680adbcb28f007ce2098561b79a487a3d784573d
slucter <slucter133@gmail.com>
2020-08-20 07:21:13 +0700

    port change

--- app.js
+++ app.js
@@ -12,4 +12,4 @@ app.use(bodyParser.urlencoded({
 
 app.use(bodyParser.json({}));
 app.use('/api/library', cors(), router);
-app.listen(2000, ()=> console.log('listening...'))
+app.listen(7878, ()=> console.log('listening...'))
